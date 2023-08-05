658d75a02a436675df91518e8f59a2c910f532ce
Eric Gip <eg@Erics-MBP.attlocal.net>
2023-08-05 02:40:55 -0700

    learned next client vs server render, added hooks to createRecipe page; maybe consider redux if we have this many states or getters and setters to handle like this

--- client/app/createRecipe/page.tsx
+++ client/app/createRecipe/page.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useState, ChangeEvent } from "react";
+
 import Button from "@mui/material/Button"
 import TextField from "@mui/material/TextField"
 import styles from "./createRecipe.module.css"
@@ -7,7 +11,49 @@ import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 
+
 export default function createRecipe() {
+
+    const [title, setTitle] = useState("Recipe name");
+    const [description, setDescription] = useState("Description");
+    const [serving, setServing] = useState("Servings");
+    const [ingredients, setIngredients] = useState("Ingredients");
+    const [steps, setSteps] = useState("Steps");
+    const [notes, setNotes] = useState("Notes");
+
+
+    function handleTitleChange(e: ChangeEvent<HTMLInputElement>) {
+        setTitle(e.target.value); 
+
+    }
+
+    function handleDescriptionChange(e: ChangeEvent<HTMLInputElement>) {
+        setDescription(e.target.value); 
+
+    }
+
+    function handleServingChange(e: ChangeEvent<HTMLInputElement>) {
+        setServing(e.target.value); 
+
+    }
+
+    function handleIngredientsChange(e: ChangeEvent<HTMLInputElement>) {
+        setIngredients(e.target.value); 
+        console.log(ingredients);
+    }
+
+    function handleStepsChange(e: ChangeEvent<HTMLInputElement>) {
+        setSteps(e.target.value); 
+        console.log(steps);
+    }
+
+    function handleNoteChange(e: ChangeEvent<HTMLInputElement>) {
+        setNotes(e.target.value); 
+        console.log(notes);
+    }
+
+
+
     return (
         <div>
             <Button variant="contained"> Save  </Button>
@@ -23,24 +69,24 @@ export default function createRecipe() {
                     id="demo-simple-select"
                     label="Private"
                 >
-                    <MenuItem value={10}>Private</MenuItem>
-                    <MenuItem value={20}>Friends list</MenuItem>
-                    <MenuItem value={30}>Public</MenuItem>
+                    <MenuItem >Private</MenuItem>
+                    <MenuItem >Friends list</MenuItem>
+                    <MenuItem >Public</MenuItem>
                 </Select>
             </FormControl>
 
 
-            <TextField className={styles.textBox} id="" label="Title" variant="outlined" />
+            <TextField className={styles.textBox} id="outlined-basic" label="Title" variant="outlined" onChange={handleTitleChange} />
             <br></br>
-            <TextField className={styles.textBox} id="outlined-basic" label="Description" variant="outlined" />
+            <TextField className={styles.textBox} id="outlined-basic" label="Description" variant="outlined" onChange={handleDescriptionChange} />
             <br></br>
-            <TextField className={styles.textBox} id="outlined-basic" label="Servings" variant="outlined" />
+            <TextField className={styles.textBox} id="outlined-basic" label="Servings" variant="outlined" onChange={handleServingChange} />
             <br></br>
-            <TextField className={styles.textBox} id="outlined-basic" label="Ingredients" variant="outlined" />
+            <TextField className={styles.textBox} id="outlined-basic" label="Ingredients" variant="outlined" onChange={handleIngredientsChange} />
             <br></br>
-            <TextField className={styles.textBox} id="outlined-basic" label="Steps" variant="outlined" />
+            <TextField className={styles.textBox} id="outlined-basic" label="Steps" variant="outlined" onChange={handleStepsChange} />
             <br></br>
-            <TextField className={styles.textBox} id="outlined-basic" label="Notes" variant="outlined" />
+            <TextField className={styles.textBox} id="outlined-basic" label="Notes" variant="outlined" onChange={handleNoteChange} />
 
             <Button variant="contained"> Save  </Button>
             <Button variant="contained"> Cancel </Button>
